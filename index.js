241ba5ff2c34e9eb87233d9b8dbcff0ceaec2162
fullmoon-sg <mbfoo7757@gmail.com>
2020-12-16 14:20:01 +0000

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const MongoUtil = require('./MongoUtil.js')
 const ObjectId = require('mongodb').ObjectId;
-
 
 require('dotenv').config();
 
 const app = express();
+app.use(cors())
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:false}))
