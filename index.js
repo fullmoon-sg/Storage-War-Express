56e1697f1f8dc2961f165e61cda77ba8f6acd241
fullmoon-sg <mbfoo7757@gmail.com>
2020-12-16 07:47:20 +0000

    add express

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 const express = require('express');
 const MongoUtil = require('./MongoUtil.js')
 const ObjectId = require('mongodb').ObjectId;
-const hbs = require('hbs')
-const wax = require('wax-on')
+
 
 require('dotenv').config();
 
 const app = express();
-app.set('view engine', 'hbs');
+
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:false}))
-
-wax.on(hbs.handlebars);
-wax.setLayoutPath('./views/layouts');
-
-var helpers = require("handlebars-helpers")({
-    handlebars: hbs.handlebars
-});
 
 async function main(){
 
@@ -26,9 +18,7 @@ let db= MongoUtil.getDB();
 
 app.get('/', async (req,res) => {
     let hall_of_fame = await db.collection('hall_of_fame').find().toArray();
-    res.render('hall_fame', {
-        'hall_fame' : hall_of_fame
-    })
+    res.json(hall_of_fame)
 })
 }
 
